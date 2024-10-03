42c59c5a034ad2b132df25d1412e7db95c5e81b9
Chris Mainey <c.mainey1@nhs.net>
2024-10-03 17:19:16 +0100

    Updated query from WA.

--- WL_query_20241003.sql
+++ WL_query_20241003.sql
@@ -10,33 +10,32 @@ AND		Week_Ending_Date >= '02-October-2022'
 AND		SOURCE_OF_REFERRAL in ('03', '3')
 and Referral_To_Treatment_Period_Status_Description = 'First activity - first activity in a referral to treatment period'
 
-
-
 */
+--Declarations for insertion
+declare			@WeDate date
+,				@TreatmentSpec varchar(3)
 
-declare @WeDate date
-declare @TreatmentSpec varchar(3)
+Set				@WeDate='02-October-2022'
+Set				@TreatmentSpec='110'
 
-Set		@WeDate='02-October-2022'
-Set		@TreatmentSpec='110'
-
+--remove temp table if already in existence
 drop table if exists ##BSOLtempChrisM;
+--create table for analysis
 create table ##BSOLtempChrisM (
 							PathwayIdentifier		varchar(76)
 ,							ReferralIdentifier		varchar(76)
 ,							Date_Added				date
-,							[FirstWE_PostAdded]		date
 ,							Date_Removed			date
 ,							[FirstWE_PreRemove]	date
 							)
+
+--Insert a couple of indexes to help on joins and insertion
 create clustered index cl_idx_PatRefID on ##BSOLtempChrisM (PathwayIdentifier,ReferralIdentifier)
 create nonclustered index ncl_add on ##BSOLtempChrisM(Date_Added asc)
 create nonclustered index ncl_rem on ##BSOLtempChrisM(Date_Removed asc)
 
-
-select * from ##BSOLtempChrisM
-
-insert into ##BSOLtempChrisM
+--Put Unique ID's in to the temp table....
+Insert into ##BSOLtempChrisM
 (
 			PathwayIdentifier
 ,			ReferralIdentifier
@@ -61,20 +60,13 @@ AND			[ORGANISATION_IDENTIFIER_(CODE_OF_COMMISSIONER)] IN ( '04X'
 ,																  'QHL00'
 																)
 AND		Week_Ending_Date >= @WeDate
-AND		SOURCE_OF_REFERRAL in ('03', '3')
+AND		SOURCE_OF_REFERRAL in ('03', '3') --GP referral
 and		Referral_To_Treatment_Period_Status_Description = 'First activity - first activity in a referral to treatment period'
-
-----------------------------------------------------------------------------------------
---and			[ORGANISATION_IDENTIFIER_(CODE_OF_PROVIDER)] in ('RRJ','RRJ00')
---and			PATIENT_PATHWAY_IDENTIFIER = '.000421825491'
---and			Referral_Identifier = '104243175'
-
-
 group by 	PATIENT_PATHWAY_IDENTIFIER
 ,			Referral_Identifier
-
 )
 
+--puts in now the first week_ending date that this record exists in the pathway selected in the where clauses...
 update		t1
 set			t1.Date_Added=t2.Date_Added
 from		##BSOLtempChrisM t1
@@ -108,7 +100,7 @@ on			t1.PathwayIdentifier=t2.PATIENT_PATHWAY_IDENTIFIER
 where		t1.ReferralIdentifier=t2.Referral_Identifier
 
 
---UPdate date removed 
+--Update date removed 
 update		t1
 set			t1.Date_Removed=t2.Date_Removed
 from		##BSOLtempChrisM t1
@@ -130,9 +122,9 @@ left join (	select		Date_Last_Attended as Date_Removed
 ,																			  'QHL'
 ,																			  'QHL00'
 																			)
-			AND		Week_Ending_Date >= @WeDate
-			AND		SOURCE_OF_REFERRAL in ('03', '3')
-			and		Referral_To_Treatment_Period_Status_Description = 'First activity - first activity in a referral to treatment period'
+			AND			Week_Ending_Date >= @WeDate
+			AND			SOURCE_OF_REFERRAL in ('03', '3')
+			and			Referral_To_Treatment_Period_Status_Description = 'First activity - first activity in a referral to treatment period'
 			and			Date_Last_Attended is not null
 			group by	Referral_Identifier
 ,						PATIENT_PATHWAY_IDENTIFIER
@@ -142,16 +134,13 @@ left join (	select		Date_Last_Attended as Date_Removed
 on			t1.PathwayIdentifier=t2.PATIENT_PATHWAY_IDENTIFIER
 where		t1.ReferralIdentifier=t2.Referral_Identifier
 
--------------------------------------------------------
---First W/E date pos tadded to WL
 
-
-
+---------------------------------------------------------------------------------------------------------------------------------------------
 
 update		t1
-set			t1.FirstWE_PostAdded=t2.thedate
+set			t1.FirstWE_PreRemove=t2.thedate
 from		##BSOLtempChrisM t1
-left join (	select		Min(Week_Ending_Date) as thedate
+left join (	select		MAX(Week_Ending_Date) as thedate
 ,						Referral_Identifier
 ,						PATIENT_PATHWAY_IDENTIFIER
 			from 		EAT_Reporting_BSOL.Development.WEEKLYWAITINGLIST_JB
@@ -178,41 +167,7 @@ left join (	select		Min(Week_Ending_Date) as thedate
 ) t2
 on			t1.PathwayIdentifier=t2.PATIENT_PATHWAY_IDENTIFIER
 where		t1.ReferralIdentifier=t2.Referral_Identifier
-and			t2.thedate>=t1.Date_Added
-
----------------------------------------------------------------------------------------------------------------------------------------------
-
-update		t1
-set			t1.FirstWE_PreRemove=t2.thedate
-from		##BSOLtempChrisM t1
-inner join (	select		Min(Week_Ending_Date) as thedate
-,						Referral_Identifier
-,						PATIENT_PATHWAY_IDENTIFIER
-			from 		EAT_Reporting_BSOL.Development.WEEKLYWAITINGLIST_JB
-			where		1=1
-			AND			Waiting_List_Type = 'ORTT'
-			AND			ACTIVITY_TREATMENT_FUNCTION_CODE = '110'-- @TreatmentSpec
-			AND			[ORGANISATION_IDENTIFIER_(CODE_OF_COMMISSIONER)] IN ( '04X'
-,																			  '04X00'
-,																			  '05P'
-,																			  '05P00'
-,																			  '13P'
-,																		      '13P00'
-,																			  '15E'
-,																			  '15E00'
-,																			  'QHL'
-,																			  'QHL00'
-																			)
-			--AND		Week_Ending_Date >= @WeDate
-			AND			SOURCE_OF_REFERRAL in ('03', '3')
-			and			Referral_To_Treatment_Period_Status_Description = 'First activity - first activity in a referral to treatment period'
-			group by	Referral_Identifier
-,						PATIENT_PATHWAY_IDENTIFIER
-
-) t2
-on			t1.PathwayIdentifier=t2.PATIENT_PATHWAY_IDENTIFIER
-where		t1.ReferralIdentifier=t2.Referral_Identifier
-and			t2.thedate>=t1.Date_Removed
+and			t1.Date_Removed<=t2.thedate
 
 
 
@@ -252,20 +207,5 @@ where		1=1
 group by Week_ending_date
 )
 
-
-
--- Count up
-select wk.Week_Ending_Date,
-wk.Pathways_Quantity,
-count(pt_add.Date_Added) as pt_add  ,
-count(pt_remove.Date_Removed) as pt_remove
-
-from ##BSOLtempChrisM_WeekEnding wk
-left join ##BSOLtempChrisM  pt_add ON wk.Week_Ending_Date = pt_add.Date_Added
-left join ##BSOLtempChrisM  pt_remove ON wk.Week_Ending_Date = pt_add.FirstWE_PreRemove
-group by wk.Week_Ending_Date,
-wk.Pathways_Quantity
-
+select *, datename(weekday,Week_Ending_Date) as thedow from ##BSOLtempChrisM_WeekEnding
 order by 1
-
-Select * from ##BSOLtempChrisM 
